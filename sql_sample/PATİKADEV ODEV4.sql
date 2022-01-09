3f8684a18478caa2d15ca844a6f52eef37ecb110
Ruhi Mert Aksu <96263914+mrtaksuuu@users.noreply.github.com>
2022-01-09 21:18:06 +0300

    Add files via upload

---
+++ sql_sample/PATİKADEV ODEV4.sql
@@ -0,0 +1,5 @@
+SELECT DISTINCT replacement_cost FROM film;
+SELECT COUNT (DISTINCT replacement_cost) FROM film;
+SELECT title FROM film WHERE title LIKE 'T%' AND rating = 'G';
+SELECT COUNT(*) FROM country WHERE country LIKE '_____' ;
+SELECT* FROM city WHERE city  LIKE  '%R' OR city LIKE '%r' ;
